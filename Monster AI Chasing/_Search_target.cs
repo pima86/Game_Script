76c65387105a13ccb897c9ac344625604bc8e1a7
Seonghyuk Kim <71416955+pima86@users.noreply.github.com>
2024-03-19 03:31:10 +0900

    Update _Search_target.cs

--- Monster AI Chasing/_Search_target.cs
+++ Monster AI Chasing/_Search_target.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Search_target : MonoBehaviour
 {
+    /*
+     * 가장 가까운 대상을 찾아서 요청하는 스크립트에게 전송합니다.
+     */
+
     public float radius = 0f;
 
     public LayerMask layer;
@@ -14,13 +18,19 @@ public class Search_target : MonoBehaviour
 
     void Update()
     {
+        colliders = null;
         colliders = Physics.OverlapSphere(transform.position, radius, layer);
 
         if (colliders.Length > 0)
         {
+            short_enemy = colliders[0];
+
             float short_distance = Vector3.Distance(transform.position, colliders[0].transform.position);
             foreach (Collider col in colliders)
             {
+                if (col.gameObject == gameObject)
+                    continue;
+
                 float short_distance2 = Vector3.Distance(transform.position, col.transform.position);
                 if (short_distance > short_distance2)
                 {
@@ -28,12 +38,9 @@ public class Search_target : MonoBehaviour
                     short_enemy = col;
                 }
             }
-            if (short_enemy == null)
-                short_enemy = colliders[0];
         }
-        else
+        else if(colliders.Length == 0)
             short_enemy = null;
-
     }
 
     private void OnDrawGizmos()
