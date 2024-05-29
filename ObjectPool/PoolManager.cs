6ca6ade0eb277b84908ed68d9d9d4798ad48921a
Seonghyuk Kim <71416955+pima86@users.noreply.github.com>
2024-05-30 02:37:57 +0900

    Create PoolManager.cs

---
+++ ObjectPool/PoolManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class Pool_Manager : MonoBehaviour
+{
+    private void Awake()
+    {
+        Init();
+    }
+
+    #region 오브젝트 풀링
+    public IObjectPool<GameObject> Pool { get; private set; }
+    public GameObject prefab;
+
+    public int defaultCapacity = 5;
+    public int maxPoolSize = 5;
+
+    private void Init()
+    {
+        Pool = new ObjectPool<GameObject>(CreatePooledItem, OnTakeFromPool, OnReturnedToPool, OnDestroyPoolObject, true, defaultCapacity, maxPoolSize);
+
+        // 미리 오브젝트 생성 해놓기
+        for (int i = 0; i < defaultCapacity; i++)
+        {
+            Pool_Obj obj = CreatePooledItem().GetComponent<Pool_Obj>();
+            obj.Pool.Release(obj.gameObject);
+        }
+    }
+
+    // 생성
+    private GameObject CreatePooledItem()
+    {
+        GameObject poolGo = Instantiate(prefab, transform);
+        poolGo.GetComponent<Pool_Obj>().Pool = this.Pool;
+        return poolGo;
+    }
+
+    // 사용
+    private void OnTakeFromPool(GameObject poolGo)
+    {
+        poolGo.SetActive(true);
+    }
+
+    // 반환
+    private void OnReturnedToPool(GameObject poolGo)
+    {
+        poolGo.SetActive(false);
+    }
+
+    // 삭제
+    private void OnDestroyPoolObject(GameObject poolGo)
+    {
+        Destroy(poolGo);
+    }
+    #endregion
+}
